fc2ab575e405342780796c9ee7b347bd8b547246
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2025-03-30 19:32:45 -0300

    ajeitando rota do cta

--- src/sections/Compartilha/CTA.jsx
+++ src/sections/Compartilha/CTA.jsx
@@ -1,15 +1,22 @@
 import { FaArrowRight } from 'react-icons/fa';
 import styles from './CTA.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const CTA = () => {
+  const navigate = useNavigate();
+
+  const handleOrçamentoClick = () => {
+    navigate("/solicitar-orcamentos")
+  };
+
   return (
     <div className={styles.fim}>
       <div className={styles.ctaContainer}>
         <h2 className={styles.ctaTitle}>Pronto para transformar seu projeto em realidade?</h2>
         <p className={styles.ctaSubtitle}>Não deixe para depois - vamos criar algo incrível juntos!</p>
-        <a href="/solicitar-orcamentos" className={styles.ctaButton}>
+        <button onClick={handleOrçamentoClick} className={styles.ctaButton}>
           Solicitar Orçamento Agora <FaArrowRight className={styles.ctaIcon} />
-        </a>
+        </button>
       </div>
       <div className={styles["voltar-para-topo"]}>
         <a href="/">Voltar para o topo</a>
